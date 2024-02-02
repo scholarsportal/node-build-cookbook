b924e644183eea617efc5225fbae868c13492e69
Mohana Sarmiento <mo.sarmiento@utoronto.ca>
2024-02-02 18:24:14 -0500

    Local test

--- resources/plugin_install.rb
+++ resources/plugin_install.rb
@@ -21,13 +21,33 @@ action :install do
     mode '0755'
   end
 
-  git new_resource.node_build_root do
-    repository new_resource.git_url
-    revision new_resource.git_revision
-    user new_resource.owner if new_resource.property_is_set?(:owner)
+  # git new_resource.node_build_root do
+  #   repository new_resource.git_url
+  #   revision new_resource.git_revision
+  #   user new_resource.owner if new_resource.property_is_set?(:owner)
+  #   group new_resource.group if new_resource.property_is_set?(:group)
+  #   enable_checkout false
+  #   action :sync
+  # end
+
+  directory new_resource.node_build_root do
+    owner new_resource.owner if new_resource.property_is_set?(:owner)
     group new_resource.group if new_resource.property_is_set?(:group)
-    enable_checkout false
-    action :sync
+  end
+
+  remote_file "v4.9.137.tar.gz" do
+    source "https://nexus.scholarsportal.info/repository/github/nodenv/node-build/archive/refs/tags/v4.9.137.tar.gz"
+    owner new_resource.owner if new_resource.property_is_set?(:owner)
+    group new_resource.group if new_resource.property_is_set?(:group)
+    mode '0644'
+    notifies :run, 'execute[extract_node_build_tar]', :immediately
+    action :create
+  end
+
+  execute 'extract_node_build_tar' do
+    command "gunzip -c v4.9.137.tar.gz| tar -xf - -C #{new_resource.node_build_root} "\
+    '--strip-components 1'
+    action :nothing
   end
 end
 
